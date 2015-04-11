08d723d8d6ecb02ca4330e04e7e653278029c3de
Carl <carl@lift3d.co>
2015-04-11 01:51:00 -0400

    got a bunch of crap working

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,27 +1,102 @@
-describe('test my test', function() {
-	it('says hello world', function() {
-		expect(test()).toBe('Hello, World');
-	});
+describe('test my test', function () {
+    it('says hello world', function () {
+        expect(test()).toBe('Hello, World');
+    });
 });
 
 
-describe('Expression evaluator', function() {
-	it('expects stringtolist to return true', function() {
-		expect(stringtolist('1+1')).toEqual(['1', '+', '1']);
-		expect(stringtolist('11 + 345 - 55')).toEqual(['11', '+', '345', '-', '55']);
-	});
+describe('Expression evaluator', function () {
+    it('expects stringtolist to return a list of elements', function () {
+        expect(stringtolist('1+1')).toEqual(['1', '+', '1']);
+        expect(stringtolist('11 + 345 - 55')).toEqual(['11', '+', '345', '-', '55']);
+    });
 
-	it(' takes a list and returns a result', function() {
-		expect(listToNumber(stringtolist('1+1'))).toEqual(2);
+    it(' takes a list of 2 and returns a result', function () {
+        var testList = stringtolist('1+1');
+        expect(listToNumber(testList)).toEqual(2);
 
-		it('takes two numbers and an operator and returns a result', function() {
-			expect(smoothOperator(2, 2, '+')).toBe(4);
-		});
-	});
+    });
+
+    it(' takes a list of 3  and returns a result', function () {
+        var testList = stringtolist('1+1+3');
+        expect(listToNumber(testList)).toEqual(5);
+
+    });
+
+    it(' takes a list of 10  and returns a result', function () {
+        var testList = stringtolist('1+1+3+4+5+6+7+8+9+10');
+        expect(listToNumber(testList)).toEqual(54);
+
+    });
+
+    it('tests Minus', function () {
+        var testList = stringtolist('2-1');
+        expect(listToNumber(testList)).toEqual(1);
+    });
+
+    it('tests Multiplication', function () {
+        var testList = stringtolist('2*10');
+        expect(listToNumber(testList)).toEqual(20);
+    });
+
+    it('test multiplication with spaces', function () {
+        var testList = stringtolist('20 * 5');
+        expect(listToNumber(testList)).toEqual(100);
+    });
+
+    it('tests Division', function () {
+        var testList = stringtolist('20/5');
+        expect(listToNumber(testList)).toEqual(4);
+    });
+
+    it('test division with spaces', function () {
+        var testList = stringtolist('20/ 5');
+        expect(listToNumber(testList)).toEqual(4);
+    });
+
+    it('takes two numbers and an operator and returns a result', function () {
+        expect(smoothOperator(2, 2, '+')).toBe(4);
+    });
+
+    it('takes a single negative number  in a string and returns a negative nubmer ', function () {
+        expect(listToNumber(['-', '123'])).toEqual(-123);
+    });
+
+    it('tests order of operations', function () {
+        var testList = stringtolist('2 /2+3 * 4.75- -6');
+        expect(listToNumber(testList)).toEqual(21.25);
+    });
+
+
 });
 
-describe('isOperator', function(){
-	it('take a pluse sign and returns true', function(){
-		expect(isOperator('+')).toBe(true);
-	});
+describe('isOperator', function () {
+    it('take a pluse sign and returns true', function () {
+        expect(isOperator('+')).toBeTruthy();
+        expect(isOperator('-')).toBeTruthy();
+        expect(isOperator('/')).toBeTruthy();
+        expect(isOperator('*')).toBeTruthy();
+        expect(isOperator('r')).toBeFalsy();
+
+    });
+});
+
+describe('list cleaner', function () {
+    it('rectifies double negatives and makes the positive', function () {
+        expect(doubleOperatorListCleaner(['2', '-', '-', '2'])).toEqual(['2', '+', '2']);
+    });
+
+    it('rectifies differing operators to a negative', function () {
+        expect(doubleOperatorListCleaner(['2', '+', '-', '2'])).toEqual(['2', '-', '2']);
+    });
+});
+
+describe('order operations', function () {
+    it('performs multiplication before addition or subtraction', function () {
+       expect(multiplicationListReducer(['2','+','2','*','8'])).toEqual(['2','+','16']);
+
+    });
+    it('performs division before addition or subtraction', function () {
+       expect(multiplicationListReducer(['2','+','8','/','2'])).toEqual(['2','+','4']);
+    });
 });
--- src/app.js
+++ src/app.js
@@ -1,64 +1,150 @@
 function test() {
-  return "Hello, World";
+    return "Hello, World";
 }
 
 
 function isOperator(char) {
-  var reg = /[+-/*]/;
-  return reg.exec(char).isNotNull();
+    var reg = /[+-/*]/;
+    return !!reg.exec(char);
+
 }
 
 
-var stringtolist = function(expression) {
-  // evaluate `expression` and return result
-  var stagedOperand = "";
-  var collectionList = [];
+var stringtolist = function (dirtyExpression) {
+    // evaluate `expression` and return result
+    var stagedOperand = "";
+    var collectionList = [];
 
-  for (var i = 0; i < expression.length; i++) {
-    if (parseInt(expression[i])) {
-      stagedOperand += expression[i];
+    var expression = dirtyExpression.replace(" ", "");
 
-    } else {
-      collectionList.push(stagedOperand);
-      stagedOperand = "";
+    for (var i = 0; i < expression.length; i++) {
+        if (parseInt(expression[i]) || expression[i] === "0" || expression[i] === ".") {
+            stagedOperand += expression[i];
+
+        } else {
+            collectionList.push(stagedOperand);
+            stagedOperand = "";
+        }
+
+        if (isOperator(expression[i])) {
+            collectionList.push(expression[i]);
+        }
     }
+    collectionList.push(stagedOperand);
 
-    if (isOperator(expression[i])) {
-      collectionList.push(expression[i]);
-    }
-  }
-  collectionList.push(stagedOperand);
+    return collectionList.filter(function (str) {
+        return /\S/.test(str);
+    });
 
-  return collectionList.filter(function(str) {
-    return /\S/.test(str);
-  });
 
 };
 
+function doubleOperatorListCleaner(dirtyList) {
+    var list = dirtyList;
+    var prefixOperator = '';
+    for (var i = 0; i < list.length; i++) {
+        if (isOperator(list[i]) && (list[i] === '+' || list[i] === '-')) {
+            if (prefixOperator) {
+                if (prefixOperator === list[i]) {
+                    list[i] = '+';
+                    list.splice(i - 1, 1);
+                } else {
+                    list[i] = '-';
+                    list.splice(i - 1, 1);
+                }
+            } else {
+                prefixOperator = list[i];
+            }
+        } else {
+            prefixOperator = '';
+        }
+    }
+    return list;
+}
+
+
 function smoothOperator(op1, op2, operator) {
-  if (operator === '+') {
-    return op1 + op2;
-  }
+    if (operator === '+') {
+        return op1 + op2;
+    }
+
+    if (operator === '-') {
+        return op1 - op2;
+    }
+    if (operator === '*') {
+        return op1 * op2;
+    }
+
+    if (operator === '/') {
+        return op1 / op2;
+    }
 
 
 }
 
-function listToNumber(list) {
-  var result = 0;
-  var op1 = '';
-  var op2 = '';
-  var operator = '';
-  for (var i = 0; i < list.length; i++) {
-    if (op1, op2, operator) {
-      op1 = smoothOperator(op1, op2, operator);
-    }
+function multiplicationListReducer(dirtyList) {
+    var list = dirtyList;
+    for(var i = 0; i < list.length; i++){
+        if(isOperator(list[i]) && (list[i]) === '*' || list[i] === '/'){
+            var op1  = list[i - 1];
+            var op2  = list[i + 1];
 
-    if (op1) {
-      if (parseInt(list[i])) {
-        op2 = parseInt(list[i]);
-      } else if (isOperator(list[i])) {
+            var result = smoothOperator(op1, op2, list[i]);
+            list[i -1] = result.toString();
 
-      }
+            list.splice(i,2);
+        }
     }
-  }
+    return list;
 }
+
+
+function listToNumber(dirtylist) {
+    var list = doubleOperatorListCleaner(dirtylist);
+    var op1 = '';
+    var op2 = '';
+    var operator = '';
+    var prefix = '';
+    for (var i = 0; i < list.length; i++) {
+        if (op1 && op2 && operator) {
+            op1 = smoothOperator(op1, op2, operator);
+            op2 = '';
+            operator = '';
+        }
+
+        if (op1) {
+            if (parseInt(list[i])) {
+                op2 = parseInt(list[i]);
+            } else if (isOperator(list[i])) {
+                operator = list[i];
+            }
+        } else if (isOperator(list[i]) && !op1) {
+
+            prefix = list[i];
+
+        } else if (prefix) {
+
+            op1 = prefix + list[i];
+            op1 = parseInt(op1);
+
+        } else {
+
+            op1 = parseInt(list[i]);
+        }
+    }
+
+    var result = op1;
+
+    if (op1 && op2 && operator) {
+        result = smoothOperator(op1, op2, operator);
+    }
+
+
+    return result;
+}
+
+doubleOperatorListCleaner(['2', '-', '-', '2']);
+var x = stringtolist('2 /2+3 * 4.75- -6');
+listToNumber(x);
+
+multiplicationListReducer(['2','+','2','*','8']);
