5033ed15fa751db2e9e519f10696d1143f1e3d88
Nemesisesq <nemesisesq@gmail.com>
2015-04-15 17:07:44 -0400

    started simple interpreter kata

--- spec/simpleInterpreterSpec.js
+++ spec/simpleInterpreterSpec.js
@@ -1,16 +1,52 @@
 var interpreter = new Interpreter();
 
-describe('simple interpreter', function () {
+describe('simple interpreter basic arithmetic', function () {
     it('takes in an expression and returns a result', function () {
-        expect(interpreter.input("1+1")).toEqual(2);
+        expect(interpreter.input("1 + 1")).toEqual(2);
+        expect(interpreter.input("2 - 1")).toEqual(1);
+        expect(interpreter.input("2 * 3")).toEqual(6);
+        expect(interpreter.input("8 / 4")).toEqual(2);
+        expect(interpreter.input("7 % 4")).toEqual(3);
+        expect(interpreter.input("20 + 1")).toEqual(21);
     });
 });
 
+describe('simple interpreter variable assignment', function () {
+    it('take in an expression that assigns a variable', function () {
+        expect(interpreter.input("x = 1")).toEqual(1);
+    });
+
+    it('it receives a variable and returns the value of the variable', function () {
+        expect(interpreter.input("x")).toEqual(1);
+    });
+
+    it('receives an expression with a variable and returns value', function () {
+        expect(interpreter.input("x + 3")).toEqual(4);
+    });
+
+    it('receives a single variable and returns and error', function () {
+        expect(function () {
+            interpreter.input("y");
+        }).toThrow(new Error('parsing is not possible'));
+    });
+
+    it('receives an empty input and handle it', function () {
+        expect(interpreter.input("")).toBe("");
+    });
+
+    it('should continue to function if an error is thrown', function () {
+        expect(interpreter.input("x = 7 ")).toBe(7);
+        expect(interpreter.input("y=x+5")).toBe(12);
+        expect(interpreter.input("y")).toBe(12);
+    });
+
+});
+
 describe('sortVarFunc', function () {
-   it('takes in a list of tokens and sorts the token into functions and variables', function(){
-       interpreter.sortVarsFunc(['1','+','1']);
+    it('takes in a list of tokens and sorts the token into functions and variables', function () {
+        interpreter.parseToken(['1', '+', '1']);
         expect(interpreter.functions[0]).toEqual("additions:[['1','+','1']]");
-   });
+    });
 });
 
 
--- src/simpleInterpreter.js
+++ src/simpleInterpreter.js
@@ -1,49 +1,137 @@
-function Interpreter()
-{
+function Interpreter() {
     this.vars = {};
     this.functions = {};
 }
 
-Interpreter.prototype.tokenize = function (program)
-{
-    if (program === ""){
+Interpreter.prototype.tokenize = function (program) {
+    if (program === "") {
 
         return [];
     }
 
     var regex = /\s*([-+*\/\%=\(\)]|[A-Za-z_][A-Za-z0-9_]*|[0-9]*\.?[0-9]+)\s*/g;
-    return program.split(regex).filter(function (s) { return !s.match(/^\s*$/); });
+    return program.split(regex).filter(function (s) {
+        return !s.match(/^\s*$/);
+    });
 };
 
-Interpreter.prototype.input = function (expr)
-{
+Interpreter.prototype.input = function (expr) {
     var tokens = this.tokenize(expr);
 
-    if(/[^\d+-/*%\s]/g.test(tokens)){
-        this.sortVarsFunc(tokens)
+    if (tokens.length === 0) {
+        return "";
+    }
+
+    if (tokens.length === 1) {
+        if (this.vars[tokens[0]]) {
+
+            return this.vars[tokens[0]];
+        } else {
+            throw new Error('parsing is not possible');
+        }
+    }
+
+    if (/[^\d+-/*%\s]/g.test(tokens)) {
+
+        var output = this.parseToken(tokens);
+        return output;
     } else {
         return this.calc(tokens.join(''));
     }
+
+
 };
 
-Interpreter.prototype.doOperation = function (op1, op2, op3){
+Interpreter.prototype.parseToken = function (tokens) {
+    this.evalToken = false;
 
+    for (var i = 0; i < tokens.length; i++) {
+        if (/=/.test(tokens[i]) && this.vars[tokens[i - i]]) {
+            this.vars[tokens[i - 1]] = tokens[i + 1];
+        }
+    }
+
+    for (var i = 0; i < tokens.length; i++) {
+
+        if (this.vars[tokens[i]]) {
+
+            tokens[i] = this.vars[tokens[i]];
+        }
+    }
+
+    if (!/[^\d\s+-/*%()]/g.test(tokens.join(''))) {
+        this.evalToken = true;
+    }
+
+    if (this.evalToken) {
+        return this.calc(tokens.join(''))
+    }
+
+    var key = "";
+    var value = 0;
+    for (var i = 0; i < tokens.length; i++) {
+
+        if (/=/.test(tokens[i])) {
+            key = tokens[i - 1];
+            value = tokens.slice(i+1 , tokens.length);
+            this.vars[key] = value;
+
+        }
+    }
+    return this.vars[key];
 };
 
-
-Interpreter.prototype.sortVarsFunc = function(tokens) {
-
-};
 
 Interpreter.prototype.calc = function (expression) {
-  var tokens = expression.match(/\d+\.\d+|\d+|[-+*/\(\)]/g).map(function(t){ return isNaN(t) ? t : Number(t); });
-  function accept(sym){ return (tokens[0] == sym) && tokens.shift() }
-  function acceptNumber(){ return !isNaN(tokens[0]) && tokens.shift() }
-  function acceptAny(arr){ return arr.some( function(a){ return a == tokens[0]} ) && tokens.shift() }
-  function doOp(x, op, y){ return [function(a,b){ return a + b;}, function(a,b){ return a - b; }, function(a,b){ return a * b; }, function(a,b){ return a / b; }][("+-*/".indexOf(op))](x,y); }
-  function unit(){ return accept('(') ? (e = expr(), accept(')'), e) : acceptNumber(); }
-  function unary(){ return accept('-') ? -unit() : unit(); }
-  function factor(){ for (var x = unary(); op = acceptAny(['*','/']); x = doOp(x, op, unary())); return x; }
-  function expr(){ for (var x = factor(); op = acceptAny(['+','-']); x = doOp(x, op, factor())); return x; }
-  return expr();
+    var tokens = expression.match(/\d+\.\d+|\d+|[-+*%/\(\)]/g).map(function (t) {
+        return isNaN(t) ? t : Number(t);
+    });
+
+    function accept(sym) {
+        return (tokens[0] == sym) && tokens.shift()
+    }
+
+    function acceptNumber() {
+        return !isNaN(tokens[0]) && tokens.shift()
+    }
+
+    function acceptAny(arr) {
+        return arr.some(function (a) {
+                return a == tokens[0]
+            }) && tokens.shift()
+    }
+
+    function doOp(x, op, y) {
+        return [function (a, b) {
+            return a + b;
+        }, function (a, b) {
+            return a - b;
+        }, function (a, b) {
+            return a * b;
+        }, function (a, b) {
+            return a % b
+        }, function (a, b) {
+            return a / b;
+        }][("+-*%/".indexOf(op))](x, y);
+    }
+
+    function unit() {
+        return accept('(') ? (e = expr(), accept(')'), e) : acceptNumber();
+    }
+
+    function unary() {
+        return accept('-') ? -unit() : unit();
+    }
+
+    function factor() {
+        for (var x = unary(); op = acceptAny(['*', '/', '%']); x = doOp(x, op, unary()));
+        return x;
+    }
+
+    function expr() {
+        for (var x = factor(); op = acceptAny(['+', '-']); x = doOp(x, op, factor()));
+        return x;
+    }
+
+    return expr();
 };
